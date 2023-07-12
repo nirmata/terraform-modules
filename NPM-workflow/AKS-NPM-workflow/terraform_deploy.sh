9a8c52c3a1477aeb21b88394d70cfab46c81fd9e
vikash <vikash@nirmata.com>
2023-07-12 20:42:28 +0530

    update the script aligned with the pipeline workflow

--- NPM-workflow/AKS-NPM-workflow/terraform_deploy.sh
+++ NPM-workflow/AKS-NPM-workflow/terraform_deploy.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-#function to handle interrupt signal
-interrupt_handler() {
-echo "Script interrupted by user."
-exit 1
-}
-
-#trap interrupt signal
-trap interrupt_handler SIGINT
 
 #initialize Terraform
 terraform init -upgrade
@@ -23,15 +15,29 @@ terraform apply -target module.n4k_module.nirmata_cluster_registered.aks-registe
 #execute Terraform plan
 terraform plan
 
-#apply all Terraform changes and auto-approve
-terraform apply --auto-approve
+# Set the maximum number of retries
+max_retries=2
+retry_count=0
 
-#check if exit status is not 0, retry Terraform apply
-while [ $? -ne 0 ]
+# Retry loop
+while [ $retry_count -lt $max_retries ]
 do
-echo "Terraform apply failed. Retrying..."
-sleep 5
+    echo "Running Terraform apply..."
+    terraform apply --auto-approve
 
-terraform apply --auto-approve
+    # Check the exit status
+    if [ $? -eq 0 ]; then
+        echo "Terraform apply successful."
+        break
+    else
+        echo "Terraform apply failed. Retrying..."
+        ((retry_count++))
+        sleep 5
+    fi
 done
-echo "Terraform apply successful."
+
+# Check if the maximum number of retries reached
+if [ $retry_count -eq $max_retries ]; then
+    echo "Terraform apply failed after $max_retries retries."
+fi
+
